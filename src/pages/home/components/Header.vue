3cb2cc3ecf4b683a0aa73b8988b671b2acd3a85a
pjqdyd <19090225079@qq.com>
2018-11-19 16:37:21 +0800

    --header相关 (使用iconfont图标字体)--

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header">
+    <div class="header-left">返回</div>
+    <div class="header-input">请输入城市/景点/游玩主题</div>
+    <div class="header-right">城市</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeHeader"
+  }
+</script>
+
+<style scoped lang="stylus" type="text/stylus">
+  .header
+    display: flex
+    line-height: .86rem
+    background: #00bcd4
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+    .header-input
+      flex: 1
+      height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      background: #fff
+      boder-radius: .1rem
+      color: #ccc
+      line-height: .64rem
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+
+</style>
