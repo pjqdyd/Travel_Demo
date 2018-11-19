a6270858bbfc8cd874be3419c5f3f60e9ebe80c7
pjqdyd <19090225079@qq.com>
2018-11-19 16:37:21 +0800

    --使用iconfont图标字体--

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <div>home</div>
+    <home-header></home-header>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
+  import HomeHeader from './components/Header'
+
+  export default {
+    name: "Home",
+    components: {
+      HomeHeader
     }
+  }
 </script>
 
 <style>
